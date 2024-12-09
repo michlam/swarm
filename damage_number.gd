36caccc8a32120c5aa51d54e77d1ded1b6bc79d4
UBC Student <lam.michael.81201@gmail.com>
2024-12-09 14:16:59 -0800

    Added damage numbers

---
+++ damage_number.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func set_values_and_animate(value, start_pos, height):
+	$Label_Container/Label.text = str(value)
+	$AnimationPlayer.play("Damage_Number")
+	
+	var tween = get_tree().create_tween()
+	var end_pos = start_pos - Vector2(0, height)
+	var tween_length = $AnimationPlayer.get_animation("Damage_Number").length
+	
+	tween.tween_property($Label_Container, "position", end_pos, tween_length).from(start_pos)
+
+func remove():
+	$AnimationPlayer.stop()
+	if is_inside_tree():
+		get_parent().remove_child(self)
+		queue_free()
--- enemy_goblin.gd
+++ enemy_goblin.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 ######################
 ## MEMBER VARIABLES ##
 ######################
+@export var damage_number_scene: PackedScene
 @export var speed = 250
+
 var screen_size
 var is_attacking = false
 var max_health = 100
@@ -21,6 +23,9 @@ func _process(delta: float) -> void:
 
 func take_damage(damage):
 	current_health -= damage
+	var damage_number = damage_number_scene.instantiate()
+	add_child(damage_number) 
+	damage_number.set_values_and_animate(damage, Vector2(0, -30), 30)
 	
 func death_handler():
 	hide()
