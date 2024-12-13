55f237cf620d9570731f99d1bf9e2871c73b1c29
UBC Student <lam.michael.81201@gmail.com>
2024-12-13 13:20:25 -0800

    Fixed some damage element logic

--- src/Enemy Goblin/element_status.gd
+++ src/Enemy Goblin/element_status.gd
@@ -2,31 +2,43 @@ extends Node
 
 # Possible element status is None, Fire, Water, and Ice.
 # Wind is not a status element, but only an applied element.
-@onready var status = "None"
+@onready var status = "Fire"
+@export var enemy_sprite: AnimatedSprite2D
 
 # There is a cooldown to elemental application. After a REACTION, there can be no application for
 # the duration of the ICD
 var on_cooldown = false
 
 func _ready() -> void:
-	pass
+	change_overlay_colour()
 
 
 # Applies the elemental reaction based on current status. 
 # Updates status, creates reaction damage nodes, updates ICD timer
 # Returns any damage multiplier
 func apply_element(applied_element: String) -> float:
-	if applied_element == "None":
+	if status == "None" && !on_cooldown:
+		if applied_element != "Wind":
+			status = applied_element
+			change_overlay_colour()
+			
 		return 1.0
-	if on_cooldown || status == applied_element || status == "None":
-		status = applied_element
+
+	if applied_element == "None" || on_cooldown:
 		return 1.0
 	else:
 		# A REACTION HAPPENED!
 		$ICD.start()
 		on_cooldown = true
+		
+		# Status should be set to none
+		status = "None"
+		change_overlay_colour()
 		return 2.0
 	
+func change_overlay_colour():
+	var color = get_element_colour(status)
+	enemy_sprite.modulate = color
 
 func get_element_colour(element: String) -> Color:
 	var color_string = "WHITE" # Physical damage
@@ -39,7 +51,6 @@ func get_element_colour(element: String) -> Color:
 		color_string = "LIGHT_STEEL_BLUE"
 	elif element == "Wind":
 		color_string = "LIGHT_SEA_GREEN"
-	
 	
 	return Color(color_string)
 
