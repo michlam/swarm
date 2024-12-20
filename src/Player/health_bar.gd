49458f89b9be4d45b18a185c15470ba10ffbce04
UBC Student <lam.michael.81201@gmail.com>
2024-12-19 19:00:41 -0800

    Clean up unneeded comments

--- src/Player/health_bar.gd
+++ src/Player/health_bar.gd
@@ -12,9 +12,3 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	position = player.position + Vector2(-25, -50)
 	value = 100.0 * (float(stats.health) / float(stats.max_health))
-
-
-#func process_healthbar_helper():
-	## Handle health bar always above player
-	#health_bar.position = $Player.position + health_bar_offset
-	
